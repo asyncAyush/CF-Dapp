d4e6caa8aa9f6debb8e544845582367ab7f453ef
asyncAyush <ayushkumar905624681@gmail.com>
2023-04-20 19:28:47 -0400

    inital commit

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -7,7 +7,7 @@ import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-  const { contract } = useContract('0xf59A1f8251864e1c5a6bD64020e3569be27e6AA9');
+  const { contract } = useContract('0xDb82d4e46bfd4C20649fB219557A0615870d798c');
   const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
 
   const address = useAddress();
